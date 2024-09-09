2a85b99bdc2ff73a5782699888c2e83ed578e2ba
weiwei <wei>
2024-09-09 15:49:55 +0800

    註解版本

--- room.js
+++ room.js
@@ -1,19 +1,17 @@
-import {
-  requireAuth,
-  sendMessageToRoom,
-  subscribeToRoom,
-  getRoom,
-  updateRoomName
-} from "./scripts/firebase";
+﻿import { requireAuth, sendMessageToRoom, subscribeToRoom } from "./scripts/firebase";
 import "./style.css";
 
+// 顯示訊息
 function appendMessage(message) {
   const messageContainer = document.getElementById("messages");
   const messageElement = document.createElement("div");
   messageElement.classList.add("message");
+  //顯示使用者
   const user = document.createElement("div");
   user.innerText = message.senderName;
   const messageText = document.createElement("div");
+
+  //判斷是否為自己發送的訊息
   if (message.isSelf) {
     user.classList.add("message__user--self");
     messageText.classList.add("message__text--self");
@@ -21,128 +19,61 @@ function appendMessage(message) {
     user.classList.add("message__user");
     messageText.classList.add("message__text");
   }
+
+  //將訊息設置為 DOM 元素
   messageText.innerText = message.content;
+  //將使用者和訊息添加到訊息元素
   messageElement.appendChild(user);
   messageElement.appendChild(messageText);
   messageContainer.appendChild(messageElement);
 }
 
+
+
+// 送出訊息
 function sendMessage(roomId) {
   const messageInput = document.getElementById("message-input");
   const message = messageInput.value;
   if (message === "") return;
 
   sendMessageToRoom(roomId, message);
-  messageInput.value = "";
+  messageInput.value = ""; //清空輸入框
 }
 
+//跳到最後訊息
 function goToBottom() {
   const messages = document.getElementById("messages");
+  // 滾動條
   messages.scrollTop = messages.scrollHeight - messages.clientHeight;
 }
 
+
+// 發送訊息
 function addSendMessageListener(roomId) {
   const sendMessageButton = document.getElementById("send-message");
   sendMessageButton.addEventListener("click", () => {
     sendMessage(roomId);
   });
 
+  // DOM 元素
   const input = document.getElementById("message-input");
+  // Enter 鍵時觸發
   input.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
       sendMessageButton.click();
     }
   });
 }
-
-function setInviteUrl() {
-  const currentUrl = window.location.href;
-  const inviteLink = document.getElementById("invite-link");
-  inviteLink.value = currentUrl;
-}
-
-function addInviteButtonListener() {
-  const inviteButton = document.getElementById("invite-button");
-  const inviteModal = document.getElementById("invite-modal");
-  setInviteUrl();
-  inviteButton.addEventListener("click", () => {
-    inviteModal.classList.add("open");
-  });
-}
-
-function addCloseInviteModalListener() {
-  const closeModalButton = document.getElementById("close-invite-modal");
-  const inviteModal = document.getElementById("invite-modal");
-  closeModalButton.addEventListener("click", () => {
-    inviteModal.classList.remove("open");
-  });
-}
-
-function addCopyButtonListener() {
-  const copyButton = document.getElementById("copy-link-btn");
-  copyButton.addEventListener("click", () => {
-    navigator.clipboard.writeText(window.location.href);
-    // set text to “以複製” for 2 seconds
-    copyButton.innerText = "已複製";
-    setTimeout(() => {
-      copyButton.innerText = "複製連結";
-    }, 2000);
-  });
-}
-
-function createTitleEditInput(value) {
-  const input = document.createElement("input");
-  input.type = "text";
-  input.value = value;
-  input.classList.add("editable-input");
-  return input;
-}
-
-function addEditIconListener(roomId) {
-  const editIcon = document.getElementById("edit-icon");
-  editIcon.addEventListener("click", () => {
-    const title = document.getElementById("chat-room-name");
-    const input = createTitleEditInput(title.textContent);
-    title.parentNode.replaceChild(input, title);
-    input.focus();
-    editIcon.style.display = "none";
-
-    function saveTitle() {
-      updateRoomName(input.value, roomId)
-        .then(() => {
-          title.textContent = input.value;
-        })
-        .finally(() => {
-          input.parentNode.replaceChild(title, input);
-          editIcon.style.display = "block";
-        });
-    }
-
-    input.addEventListener("blur", saveTitle);
-    input.addEventListener("keydown", (e) => {
-      if (e.key === "Enter") {
-        input.blur();
-      }
-    });
-  });
-}
-
-async function loadRoomName(roomId) {
-  const title = document.getElementById("chat-room-name");
-  const room = await getRoom(roomId);
-  title.textContent = room.name;
-}
-
+// 設定事件
 function setupEventListeners(roomId) {
   addSendMessageListener(roomId);
-  addInviteButtonListener();
-  addCloseInviteModalListener();
-  addCopyButtonListener();
-  addEditIconListener(roomId);
 }
+
+
 const messageIds = new Set();
+// 訊息更新
 function messageUpdateHandler(messages) {
-  messages.forEach((message) => {
+  messages.forEach(message => {
     if (!messageIds.has(message.id)) {
       messageIds.add(message.id);
       appendMessage(message);
@@ -151,9 +82,12 @@ function messageUpdateHandler(messages) {
   goToBottom();
 }
 
+// 確保使用者已經通過身份驗證
 requireAuth().then((user) => {
+  // 從 URL 查詢參數中獲取房間 ID
   const roomId = new URLSearchParams(window.location.search).get("roomId");
-  loadRoomName(roomId);
+  // 設置與房間相關的事件監聽器
   setupEventListeners(roomId);
+  // 訂閱房間的訊息更新
   subscribeToRoom(messageUpdateHandler, roomId);
 });
--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+﻿// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
   GoogleAuthProvider,
@@ -6,15 +6,7 @@ import {
   onAuthStateChanged,
   signInWithPopup
 } from "firebase/auth";
-import {
-  getFirestore,
-  addDoc,
-  collection,
-  onSnapshot,
-  doc,
-  getDoc,
-  setDoc
-} from "firebase/firestore";
+import { getFirestore, addDoc, collection, onSnapshot } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -58,28 +50,31 @@ export async function createRoom() {
     name: "Chat Room",
     createdAt: new Date()
   });
-
   return room.id;
 }
 
+// 送出訊息資料 辨識資料
 export async function sendMessageToRoom(roomId, content) {
   const db = getFirestore();
+  // 建立一個指向 Firestore 中特定房間訊息集合的引用
   const messageRef = collection(
-    db,
-    COLLECTIONS.ROOM,
-    roomId,
-    COLLECTIONS.MESSAGE
+    db, // Firestore 資料庫實例
+    COLLECTIONS.ROOM, // 房間集合的名稱
+    roomId, // 特定房間的 ID
+    COLLECTIONS.MESSAGE // 訊息集合的名稱
   );
   const auth = getAuth();
+  // 向 Firestore 中的訊息集合新增一個新訊息文件
   const message = await addDoc(messageRef, {
-    senderEmail: auth.currentUser.email,
-    senderName: auth.currentUser.displayName,
-    content,
-    timestamp: new Date()
+    senderEmail: auth.currentUser.email, // 發送者的電子郵件
+    senderName: auth.currentUser.displayName, // 發送者的顯示名稱
+    content, // 訊息內容
+    timestamp: new Date() // 訊息的時間戳記
   });
   return message;
 }
 
+// 訂閱訊息
 export async function subscribeToRoom(fn, roomId) {
   const db = getFirestore();
   const messageRef = collection(
@@ -89,32 +84,19 @@ export async function subscribeToRoom(fn, roomId) {
     COLLECTIONS.MESSAGE
   );
 
+  // 訂閱訊息集合 陣列
   const unsubscribe = onSnapshot(messageRef, (messages) => {
+    // 取得使用者 
     const auth = getAuth();
-    const transformedMessages = messages.docs.map((doc) => {
+    // 處理 訊息
+    const transformedMessages = messages.docs.map(doc => {
       const data = doc.data();
       const id = doc.id;
-      return {
-        id,
-        isSelf: auth.currentUser.email === data.senderEmail,
-        ...data
-      };
+      return { id, isSelf: auth.currentUser.email === data.senderEmail, ...data };
     });
 
     fn(transformedMessages);
   });
 
   return unsubscribe;
-}
-
-export function getRoom(roomId) {
-  const db = getFirestore();
-  const roomRef = doc(db, COLLECTIONS.ROOM, roomId);
-  return getDoc(roomRef).then((doc) => doc.data());
-}
-
-export async function updateRoomName(name, roomId) {
-  const db = getFirestore();
-  const roomRef = doc(db, COLLECTIONS.ROOM, roomId);
-  await setDoc(roomRef, { name: name }, { merge: true });
 }
